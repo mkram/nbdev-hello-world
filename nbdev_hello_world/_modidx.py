9039a0192b330edbc2bd435ab07a5966cfd4a3bf
mkram <maheshkr81@gmail.com>
2023-03-04 21:43:06 -0800

    Add say_hello; update index

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://mkram.github.io',
                 'git_url': 'https://github.com/mkram/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('mycore.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.mycore': { 'nbdev_hello_world.mycore.foo': ('mycore.html#foo', 'nbdev_hello_world/mycore.py'),
+                                          'nbdev_hello_world.mycore.say_hello': ('mycore.html#say_hello', 'nbdev_hello_world/mycore.py')}}}
